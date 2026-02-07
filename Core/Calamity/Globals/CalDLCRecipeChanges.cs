b4994769b54aca507408821bdfd7bcf2af212cd8
schmoovi <romandark1@gmail.com>
2026-02-07 16:19:12 -0600

    Added Banner Recipe for Antlion Skewer.

--- Core/Calamity/Globals/CalDLCRecipeChanges.cs
+++ Core/Calamity/Globals/CalDLCRecipeChanges.cs
@@ -520,6 +520,7 @@ namespace FargowiltasCrossmod.Core.Calamity.Globals
             AddBannerToItemRecipe(ItemID.TombCrawlerBanner, ItemType<BurntSienna>());
             AddBannerToItemRecipe(ItemID.DemonBanner, ItemType<BladecrestOathsword>());
             AddBannerToItemRecipe(ItemID.GoblinSorcererBanner, ItemType<PlasmaRod>());
+            AddBannerToItemRecipe(ItemID.AntlionBanner, ItemType<AntlionSkewer>());
             AddBannerToItemRecipe(ItemType<BoxJellyfishBanner>(),
                 ItemType<AbyssShocker>(), 1, 1, ItemID.Bone);
             AddBannerToItemRecipe(ItemType<NuclearToadBanner>(),
