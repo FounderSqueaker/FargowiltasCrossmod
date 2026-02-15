76dac198ebfa8461c1dc9b8fca66eb6665c39207
JavyzTaken <jacklukas@outlook.com>
2026-02-15 14:53:46 +0100

    updated souls dll, CursedArtifact internal rename

--- Core/Calamity/Globals/CalDLCItemChanges.cs
+++ Core/Calamity/Globals/CalDLCItemChanges.cs
@@ -345,7 +345,7 @@ namespace FargowiltasCrossmod.Core.Calamity.Globals
             }
             return false;
         }
-        public static int[] SummonsThatDontMeetConditionsButShould = [ ModContent.ItemType<FleshyDoll>(), ModContent.ItemType<MechanicalAmalgam>(), /*ModContent.ItemType<MechEye>(), */ModContent.ItemType<PortableCodebreaker>(), ModContent.ItemType<CrystallineEffigy>(), ModContent.ItemType<MechLure>(), ModContent.ItemType<CoffinSummon>(), ModContent.ItemType<DevisCurse>(), ModContent.ItemType<AbomsCurse>(), ModContent.ItemType<MutantsCurse>()];
+        public static int[] SummonsThatDontMeetConditionsButShould = [ ModContent.ItemType<FleshyDoll>(), ModContent.ItemType<MechanicalAmalgam>(), /*ModContent.ItemType<MechEye>(), */ModContent.ItemType<PortableCodebreaker>(), ModContent.ItemType<CrystallineEffigy>(), ModContent.ItemType<MechLure>(), ModContent.ItemType<CursedArtifact>(), ModContent.ItemType<DevisCurse>(), ModContent.ItemType<AbomsCurse>(), ModContent.ItemType<MutantsCurse>()];
         public override void SetDefaults(Item item)
         {
             if (Fargowiltas.Content.Items.FargoGlobalItem.AlwaysUsableVanillaSummons.Contains(item.type))
