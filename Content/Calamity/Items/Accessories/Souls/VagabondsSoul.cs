2033902da6eb38535c283f7719bcf04beedb34c6
Zavarski <hrom0405@gmail.com>
2026-03-16 23:30:08 +0800

    fixed vaga soul + loc touch-ups

--- Content/Calamity/Items/Accessories/Souls/VagabondsSoul.cs
+++ Content/Calamity/Items/Accessories/Souls/VagabondsSoul.cs
@@ -19,7 +19,6 @@ namespace FargowiltasCrossmod.Content.Calamity.Items.Accessories.Souls
     public class VagabondsSoul : BaseSoul
     {
         public override string Texture => "FargowiltasCrossmod/Content/Calamity/Items/Accessories/Souls/" + Name;
-        protected override Color? nameColor => new Color(217, 144, 67);
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetDamage<RogueDamageClass>() += 0.22f;
